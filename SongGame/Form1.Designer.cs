b53ffc82cbed519c361e0573818762f31d102b04
Munchyy <max.pickup94@gmail.com>
2016-12-23 13:05:03 +0000

    Add project files.

---
+++ SongGame/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace SongGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.songNameLabel = new System.Windows.Forms.Label();
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.restartButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 130);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(526, 207);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // songNameLabel
+            // 
+            this.songNameLabel.AutoSize = true;
+            this.songNameLabel.Location = new System.Drawing.Point(56, 42);
+            this.songNameLabel.Name = "songNameLabel";
+            this.songNameLabel.Size = new System.Drawing.Size(60, 13);
+            this.songNameLabel.TabIndex = 1;
+            this.songNameLabel.Text = "SongName";
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.Location = new System.Drawing.Point(362, 39);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(35, 13);
+            this.scoreLabel.TabIndex = 2;
+            this.scoreLabel.Text = "Score";
+            // 
+            // restartButton
+            // 
+            this.restartButton.Location = new System.Drawing.Point(12, 343);
+            this.restartButton.Name = "restartButton";
+            this.restartButton.Size = new System.Drawing.Size(94, 27);
+            this.restartButton.TabIndex = 3;
+            this.restartButton.Text = "Restart";
+            this.restartButton.UseVisualStyleBackColor = true;
+            this.restartButton.Click += new System.EventHandler(this.restartButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(532, 400);
+            this.Controls.Add(this.restartButton);
+            this.Controls.Add(this.scoreLabel);
+            this.Controls.Add(this.songNameLabel);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label songNameLabel;
+        private System.Windows.Forms.Label scoreLabel;
+        private System.Windows.Forms.Button restartButton;
+    }
+}
+
---
+++ SongGame/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SongGame
+{
+    public partial class Form1 : Form
+    {
+        private int gameScore;
+        private List<Tuple<string, string>> songList;
+
+        public Form1()
+        {
+            InitializeComponent();
+            gameScore = 0;
+        }
+
+        private void restartButton_Click(object sender, EventArgs e)
+        {
+            gameScore = 0;
+        }
+    }
+}
